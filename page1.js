2d06ff413fb040a4d6aaa15240398b8a825e732d
Harsh Yadav <harahyadav01@gmail.com>
2024-11-15 21:39:15 +0530

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,9 @@
+// Handle Car Comparison
+function startComparison() {
+    window.location.href = "#compare";
+}
+
+// Contact Dealer Alert
+function contactDealer() {
+    alert("Thank you for your interest! The Car dealer contact number : +91 98560-98560.");
+}
